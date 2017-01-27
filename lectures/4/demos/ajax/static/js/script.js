468ac632c8e788c959e09c0450b967557554c817
Thierry Sans <thierry.sans@gmail.com>
2017-01-27 09:58:49 -0500

    week 4 lecture

---
+++ lectures/4/demos/ajax/static/js/script.js
@@ -0,0 +1,59 @@
+(function(){
+    
+    var updateProfile = function(url, username){
+        var img = new Image();
+		img.src = url;
+        img.alt = username;
+        var span = document.createElement('span');
+        span.innerHTML = username;
+        var result = document.getElementById("result");
+        result.innerHTML = "";
+        result.appendChild(img);
+        result.appendChild(span);
+    };
+    
+    document.getElementsByName('picture')[0].onchange = function(e){
+		var file = e.target.files[0];
+		var imageType = /image.*/;
+		if (file.type.match(imageType)) {
+			var reader = new FileReader();
+			reader.onload = function(e) {
+                updateProfile(reader.result, "PREVIEW");
+			};
+			reader.readAsDataURL(file);	
+		}
+    }
+
+    var fetchProfile = function(){
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (this.readyState === XMLHttpRequest.DONE){
+                if (this.status === 200){
+                    var profile = JSON.parse(this.responseText);
+                    updateProfile("/api/profile/picture/", profile.username); 
+                }
+            }
+        };
+        xhr.open("GET", "/api/profile/", true);
+        xhr.send();
+    }
+    
+    document.getElementsByTagName("form")[0].onsubmit = function(e){
+        e.preventDefault();
+        var file = document.getElementsByName("picture")[0].files[0];
+        var data = {username: document.getElementsByName("username")[0].value};
+        var formdata = new FormData();
+        formdata.append("picture", file);
+        formdata.append("data", JSON.stringify(data));
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (this.readyState === XMLHttpRequest.DONE){
+                    fetchProfile();
+            }
+        };
+        xhr.open("POST", "/api/profile/", true);
+        xhr.send(formdata);
+    }
+    
+}())
+
