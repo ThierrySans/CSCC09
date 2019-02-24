952a369e82733418f8257e08b3cb65ce2a274ac6
Thierry Sans <thierry.sans@gmail.com>
2019-02-24 18:01:03 -0500

    lec6 update

--- lectures/06/src/todo/app.js
+++ lectures/06/src/todo/app.js
@@ -7,6 +7,8 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const validator = require('validator');
+
 const Datastore = require('nedb');
 var users = new Datastore({ filename: 'db/users.db', autoload: true });
 var items = new Datastore({ filename: path.join(__dirname,'db', 'items.db'), autoload: true, timestampData : true});
@@ -23,7 +25,13 @@ app.use(session({
     secret: 'please change this secret',
     resave: false,
     saveUninitialized: true,
+    cookie: {httpOnly: true, sameSite: true}
 }));
+
+// we use http for dev but https fpor production
+if (app.get('env') === 'production') {
+    session.cookie.secure = true;
+}
 
 function generateSalt (){
     return crypto.randomBytes(16).toString('base64');
@@ -36,8 +44,7 @@ function generateHash (password, salt){
 }
 
 app.use(function(req, res, next){
-    req.user = ('user' in req.session)? req.session.user : null;
-    var username = (req.user)? req.user._id : '';
+    var username = (req.session.username)? req.session.username : '';
     res.setHeader('Set-Cookie', cookie.serialize('username', username, {
           path : '/', 
           maxAge: 60 * 60 * 24 * 7 // 1 week in number of seconds
@@ -52,13 +59,29 @@ app.use(function (req, res, next){
     next();
 });
 
+
 var isAuthenticated = function(req, res, next) {
-    if (!req.user) return res.status(401).end("access denied");
+    if (!req.session.username) return res.status(401).end("access denied");
+    next();
+};
+
+var checkUsername = function(req, res, next) {
+    if (!validator.isAlphanumeric(req.body.username)) return res.status(400).end("bad input");
+    next();
+};
+
+var sanitizeContent = function(req, res, next) {
+    req.body.content = validator.escape(req.body.content);
+    next();
+}
+
+var checkId = function(req, res, next) {
+    if (!validator.isAlphanumeric(req.params.id)) return res.status(400).end("bad input");
     next();
 };
 
 // curl -X POST -d "username=admin&password=pass4admin" http://localhost:3000/signup/
-app.post('/signup/', function (req, res, next) {
+app.post('/signup/', checkUsername, function (req, res, next) {
     // extract data from HTTP request
     if (!('username' in req.body)) return res.status(400).end('username is missing');
     if (!('password' in req.body)) return res.status(400).end('password is missing');
@@ -80,7 +103,7 @@ app.post('/signup/', function (req, res, next) {
 });
 
 // curl -X POST -d "username=admin&password=pass4admin" -c cookie.txt http://localhost:3000/signin/
-app.post('/signin/', function (req, res, next) {
+app.post('/signin/', checkUsername, function (req, res, next) {
     // extract data from HTTP request
     if (!('username' in req.body)) return res.status(400).end('username is missing');
     if (!('password' in req.body)) return res.status(400).end('password is missing');
@@ -92,11 +115,7 @@ app.post('/signin/', function (req, res, next) {
         if (!user) return res.status(401).end("access denied");
         if (user.hash !== generateHash(password, user.salt)) return res.status(401).end("access denied"); // invalid password
         // start a session
-        req.session.user = user;
-        res.setHeader('Set-Cookie', cookie.serialize('username', user._id, {
-              path : '/', 
-              maxAge: 60 * 60 * 24 * 7 // 1 week in number of seconds
-        }));
+        req.session.username = user._id;
         return res.redirect("/");
     });
 });
@@ -118,14 +137,14 @@ app.get('/api/items/', function (req, res, next) {
     });
 });
 
-app.post('/api/items/', isAuthenticated, function (req, res, next) {
-    items.insert({content: req.body.content, owner: req.user._id}, function (err, item) {
+app.post('/api/items/', sanitizeContent, isAuthenticated, function (req, res, next) {
+    items.insert({content: req.body.content, owner: req.session.username}, function (err, item) {
         if (err) return res.status(500).end(err);
         return res.json(item);
     });
 });
 
-app.get('/api/items/:id/', function (req, res, next) {
+app.get('/api/items/:id/', checkId, function (req, res, next) {
     items.findOne({_id: req.params.id}, function(err, item){
         if (err) return res.status(500).end(err);
         if (!item) return res.status(404).end("Item id #" + req.params.id + " does not exists");
@@ -133,10 +152,10 @@ app.get('/api/items/:id/', function (req, res, next) {
     });    
 });
 
-app.delete('/api/items/:id/', isAuthenticated, function (req, res, next) {
+app.delete('/api/items/:id/', isAuthenticated, checkId, function (req, res, next) {
     items.findOne({_id: req.params.id}, function(err, item){
         if (err) return res.status(500).end(err);
-        if (item.owner !== req.user._id) return res.status(403).end("forbidden");
+        if (item.owner !== req.session.username) return res.status(403).end("forbidden");
         if (!item) return res.status(404).end("Item id #" + req.params.id + " does not exists");
         items.remove({ _id: item._id }, { multi: false }, function(err, num) {  
             res.json(item);
