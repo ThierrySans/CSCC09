b125bb378daeb58a42ff46e16e3c0c53f00f0e56
Thierry Sans <thierry.sans@gmail.com>
2019-12-18 13:50:28 -0500

    2020 ready

--- lectures/03/src/node/readfile.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-
-fs.readFile('helloworld.txt', 'utf8', function(err, data) {  
-    if (err) console.log(err);
-    return console.log(data);
-});
-
-console.log("hello you!");
--- lectures/06/src/dom-xss/static/js/script.js
+++
@@ -1,8 +0,0 @@
-(function(){
-    
-     window.onload = function(e){
-         var data = document.location.href.substring(document.location.href.indexOf("default=")+8);
-         document.write(data);
-     };
-}())
-
