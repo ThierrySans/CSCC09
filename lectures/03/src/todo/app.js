5ed218997d3b001cc49f4ff40ed5553c08495409
Thierry Sans <thierry.sans@gmail.com>
2021-02-02 12:00:13 -0500

    example update

--- lectures/03/src/todo/app.js
+++ lectures/03/src/todo/app.js
@@ -4,8 +4,8 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-var Item = (function(){
-    var id = 0;
+let Item = (function(){
+    let id = 0;
     return function item(item){
         this.id = id++;
         this.content = item.content;
@@ -17,44 +17,35 @@ app.use(function (req, res, next){
     next();
 });
 
-var items = [];
+let items = [];
 
 app.get('/api/items/', function (req, res, next) {
     res.json(items);
-    next();
 });
 
 app.post('/api/items/', function (req, res, next) {
-    var item = new Item(req.body);
+    let item = new Item(req.body);
     items.push(item);
     res.json(item);
-    next();
 });
 
 app.get('/api/items/:id/', function (req, res, next) {
     res.json(items[req.params.id]);
-    next();
 });
 
 app.delete('/api/items/:id/', function (req, res, next) {
-    var index = items.findIndex(function(e){
+    let index = items.findIndex(function(e){
         return (e.id === parseInt(req.params.id));
     });
     if (index === -1) res.json(null);
     else{
-        var item = items[index];
+        let item = items[index];
         items.splice(index, 1);
         res.json(item);
     }
-    next();
 });
 
 app.use(express.static('static'));
-
-app.use(function (req, res, next){
-    console.log("Storage", JSON.stringify(items, null, 2));
-    console.log("HTTP Response", res.statusCode);
-});
 
 const http = require('http');
 const PORT = 3000;
